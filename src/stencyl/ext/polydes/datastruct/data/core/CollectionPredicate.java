c3af66c98be0ef796c3efb4a9543900f6e8d5e78
Justin Espedal <justin.espedal@gmail.com>
2015-09-07 12:29:46 -0700

    Move collection observer utility to common classes.

--- src/stencyl/ext/polydes/datastruct/data/core/CollectionPredicate.java
+++
@@ -1,6 +0,0 @@
-package stencyl.ext.polydes.datastruct.data.core;
-
-public interface CollectionPredicate<T>
-{
-	public boolean test(T t);
-}
--- src/stencyl/ext/polydes/datastruct/data/core/CollectionUpdateListener.java
+++
@@ -1,6 +0,0 @@
-package stencyl.ext.polydes.datastruct.data.core;
-
-public interface CollectionUpdateListener
-{
-	public void listUpdated();
-}
