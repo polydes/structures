9561dc235b11ce60f1ea7121c2b519c551b947be
Justin Espedal <justin.espedal@gmail.com>
2017-04-30 01:37:46 -0700

    Engine reload hook

--- engine/src/com/polydes/datastruct/DataStructures.hx
+++ engine/src/com/polydes/datastruct/DataStructures.hx
@@ -5,15 +5,13 @@ package com.polydes.datastruct;
 
 class DataStructures
 {
-	private static var init:Bool = false;
+	private static var init = false;
+	private static var reloadSet = false;
 
 	public static function get(s:String):Dynamic
 	{
 		if(!init)
-		{
-			DataStructureReader.readData();
-			init = true;
-		}
+			readData();
 
 		return nameMap.get(s);
 	}
@@ -21,15 +19,32 @@ class DataStructures
 	public static function getByID(i:Int):Dynamic
 	{
 		if(!init)
-		{
-			DataStructureReader.readData();
-			init = true;
-		}
+			readData();
 
 		return idMap.get(i);
+	}
+
+	private static function readData()
+	{
+		DataStructureReader.readData();
+		init = true;
+		if(!reloadSet)
+		{
+			Universal.addReloadListener(reload);
+			reloadSet = true;
+		}
 	}
 
 	public static var idMap:Map<Int, Dynamic> = new Map<Int, Dynamic>();
 	public static var nameMap:Map<String, Dynamic> = new Map<String, Dynamic>();
 	public static var types:Map<String, String> = new Map<String, String>();
+
+	public static function reload()
+	{
+		init = false;
+		idMap = new Map<Int, Dynamic>();
+		nameMap = new Map<String, Dynamic>();
+		types = new Map<String, String>();
+		StringData.readers = new Map<String, String->Dynamic>();
+	}
 }
